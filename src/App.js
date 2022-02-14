876a8b781f01b875db065e6e726800ba6802e7db
Jonathan NARBONI <jhonjhon2802@gmail.com>
2022-02-14 16:41:21 +0100

    exo simpson

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import react, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [quote, setQuote] = useState ('');
+  const [count, setCount] = useState (0);
+
+  useEffect(() => {
+    axios
+      .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+      .then((response) => 
+        setQuote(response.data[0]))
+  }, [count])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{quote.quote}</p>
+      <p>{quote.characters}</p>
+      <img src={quote.image}></img>
+      <button type="button" onClick={() => setCount((count) => count+1)}>NewQuoteSimpsooons</button>
     </div>
   );
 }
